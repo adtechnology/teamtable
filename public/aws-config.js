f622c85b19c09dde13b6c79b65d18ee1338c7214
Carlos Bracho <carlos.bracho@axelspringer.de>
2024-06-21 12:07:02 +0200

    Create aws-config.js

---
+++ public/aws-config.js
@@ -0,0 +1,6 @@
+// Configure AWS SDK
+AWS.config.update({
+    accessKeyId: window.AWS_ACCESS_KEY_ID,
+    secretAccessKey: window.AWS_SECRET_ACCESS_KEY,
+    region: 'your-region' // ERSETZE DIESEN WERT
+});
