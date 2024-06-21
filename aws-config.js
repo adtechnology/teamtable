b5f3d5765b1f12a284d66bbca8fb56f11d9a6047
Carlos Bracho <carlos.bracho@axelspringer.com>
2024-06-21 11:45:08 +0200

    s3 bucket support

--- aws-config.js
+++ aws-config.js
@@ -1,2 +1,2 @@
-window.AWS_ACCESS_KEY_ID = 'your-access-key-id';
-window.AWS_SECRET_ACCESS_KEY = 'your-secret-access-key';
+window.AWS_ACCESS_KEY_ID = '${{ secrets.AWS_ACCESS_KEY_ID }}';
+window.AWS_SECRET_ACCESS_KEY = '${{ secrets.AWS_SECRET_ACCESS_KEY }}';
